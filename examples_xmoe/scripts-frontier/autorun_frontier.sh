8e49013949c07559847e7863f102cadf65bd11f8
Zixian Wang <zixianw4@login04.frontier.olcf.ornl.gov>
2025-12-06 23:50:21 -0500

    update auto scripts with dynamic ckpt and uneven pp

--- examples_xmoe/scripts-frontier/autorun_frontier.sh
+++ examples_xmoe/scripts-frontier/autorun_frontier.sh
@@ -17,8 +17,8 @@ MP_SIZE=1
 # (Example: PP_STRATEGY_MAP["4:32"]="4:8 8:4")
 # (Example: PP_BATCH_MAP["4:32"]="4:256:100:X-MOE:10.1")
 declare -A PP_STRATEGY_MAP
-PP_STRATEGY_MAP["1:4"]="2:2" 
-PP_STRATEGY_MAP["1:8"]="2:4" 
+PP_STRATEGY_MAP["1:4"]="4:1" 
+PP_STRATEGY_MAP["1:8"]="4:2" 
 PP_STRATEGY_MAP["2:8"]="2:4" 
 PP_STRATEGY_MAP["2:16"]="2:8" 
 PP_STRATEGY_MAP["4:16"]="4:4" 
@@ -39,12 +39,31 @@ declare -A PP_BATCH_MAP
 # PP_BATCH_MAP["2:16"]="1:96:20:X-MOE:10B"
 # PP_BATCH_MAP["4:32"]="1:1152:20:X-MOE:50B:ckpt 1:1152:20:X-MOE:50B:no-ckpt 2:1152:20:X-MOE:50B:ckpt 2:1152:20:X-MOE:50B:no-ckpt 3:1152:20:X-MOE:50B:ckpt 3:1152:20:X-MOE:50B:no-ckpt 4:1152:20:X-MOE:50B:ckpt 4:1152:20:X-MOE:50B:no-ckpt "
 # PP_BATCH_MAP["4:32"]="5:960:20:X-MOE:50B:ckpt 6:1152:20:X-MOE:50B:ckpt 7:1344:20:X-MOE:50B:ckpt 8:1344:20:X-MOE:50B:ckpt "
-PP_BATCH_MAP["60:480"]="1:1920:14:X-MOE:537B:ckpt:1  "
+# PP_BATCH_MAP["1:8"]="1:64:100:X-MOE:10B:no-ckpt:1"
+# PP_BATCH_MAP["1:4"]="2:128:20:X-MOE:10B:no-ckpt:0 2:128:20:X-MOE:10B:ckpt:1 2:128:20:X-MOE:10B:ckpt:2"
+# PP_BATCH_MAP["4:32"]="1:128:20:X-MOE:50B:no-ckpt:0 1:128:20:X-MOE:50B:ckpt:1 1:128:20:X-MOE:50B:ckpt:2   2:256:20:X-MOE:50B:no-ckpt:0 2:256:20:X-MOE:50B:ckpt:1 2:256:20:X-MOE:50B:ckpt:2"
+# PP_BATCH_MAP["1:4"]="1:128:20:X-MOE:10B:ckpt:3   2:256:20:X-MOE:10B:ckpt:3"
+# PP_BATCH_MAP["4:32"]="1:128:2:X-MOE:50B:ckpt:1 "
 
 
+# PP_BATCH_MAP["60:480"]="1:1920:14:X-MOE:537B:ckpt:1 1:1920:14:X-MOE:537B:dynamic-ckpt:1  "
+# PP_BATCH_MAP["4:32"]="1:96:3:X-MOE:50B:ckpt:1:even 1:96:3:X-MOE:50B:dynamic-ckpt:1:uneven 1:96:3:X-MOE:50B:dynamic-ckpt:1:even"
+# PP_BATCH_MAP["1:8"]="1:960:20:X-MOE:10B:ckpt:1:uneven 1:960:20:X-MOE:10B:dynamic-ckpt:1:even 1:960:20:X-MOE:10B:dynamic-ckpt:1:uneven 1:960:20:X-MOE:10B:ckpt:1:even 1:960:20:X-MOE:10B:no-ckpt:1:even "
+# PP_BATCH_MAP["4:32"]="1:960:20:X-MOE:50B:ckpt:1:uneven 1:960:20:X-MOE:50B:dynamic-ckpt:1:even 1:960:20:X-MOE:50B:dynamic-ckpt:1:uneven 1:960:20:X-MOE:50B:ckpt:1:even 1:960:20:X-MOE:50B:no-ckpt:1:even "
+# PP_BATCH_MAP["1:8"]="4:96:5:X-MOE:10B:no-ckpt:1:even 4:96:5:X-MOE:10B:ckpt:1:even "
+# PP_BATCH_MAP["4:32"]="1:96:5:X-MOE:50B:no-ckpt:1:even 1:96:5:X-MOE:50B:ckpt:1:even "
+
+# check planner throughput 
+# PP_BATCH_MAP["4:32"]="   4:1280:14:X-MOE:63B:ckpt:1:even   3:960:16:X-MOE:63B:ckpt:1:even     "
+# PP_BATCH_MAP["4:32"]="  1:720:20:X-MOE:63B:dynamic-ckpt:1:uneven   1:720:20:X-MOE:63B:ckpt:1:even  2:1440:13:X-MOE:63B:ckpt:1:even "
+# PP_BATCH_MAP["4:32"]="7:1120:20:X-MOE:50B:ckpt:1:uneven 8:1280:20:X-MOE:50B:ckpt:1:uneven 6:960:20:X-MOE:50B:ckpt:1:even"
+
+
+# PP_BATCH_MAP["1:8"]="1:96:3:X-MOE:10B:dynamic-ckpt:1:uneven"
+# PP_BATCH_MAP["1:4"]="1:8:2:X-MOE:10B:ckpt:1"
 
 # PP_BATCH_MAP["4:32"]="1:1152:20:X-MOE:50B:no-ckpt"
-# PP_BATCH_MAP["4:32"]="1:96:5:X-MOE:50B:ckpt:3"
+
 
 
 # PP_BATCH_MAP["6:48"]="1:256:30:X-MOE:190B_6L 1:256:30:X-MOE:190B"
@@ -62,6 +81,9 @@ PP_BATCH_MAP["60:480"]="1:1920:14:X-MOE:537B:ckpt:1  "
 # PP_BATCH_MAP["24:192"]="1:1152:15:X-MOE:190B:ckpt:1 2:1152:15:X-MOE:190B:ckpt:1 4:1152:15:X-MOE:190B:ckpt:1 "
 # PP_BATCH_MAP["12:96"]="1:1152:15:X-MOE:190B:ckpt:1 2:1152:15:X-MOE:190B:ckpt:1 4:1152:15:X-MOE:190B:ckpt:1     1:1152:15:X-MOE:190B:ckpt:2 2:1152:15:X-MOE:190B:ckpt:2 4:1152:15:X-MOE:190B:ckpt:2 "
 # PP_BATCH_MAP["8:64"]="1:1152:15:X-MOE:190B:ckpt:1"
+# PP_BATCH_MAP["24:192"]="1:1152:15:X-MOE:190B:ckpt:1:even 1:1152:15:X-MOE:190B:dynamic-ckpt:1:even "
+# PP_BATCH_MAP["12:96"]="1:1152:15:X-MOE:190B:ckpt:1:even 1:1152:15:X-MOE:190B:dynamic-ckpt:1:even "
+# PP_BATCH_MAP["8:64"]="1:1152:15:X-MOE:190B:ckpt:1:even 1:1152:15:X-MOE:190B:dynamic-ckpt:1:even "
 
 
 
@@ -81,6 +103,7 @@ declare -A EP_BATCH_MAP
 # EP_BATCH_MAP["16:128:2"]="1:2048:30:X-MOE:190B"
 
 
+EP_BATCH_MAP["1:8:1"]=" 1:32:15:X-MOE:173B_1L:no-ckpt:0  "
 # EP_BATCH_MAP["4:32:1"]=" 1:1024:15:X-MOE:50B:no-ckpt:0   1:1024:15:X-MOE:50B:ckpt:1 2:1024:15:X-MOE:50B:ckpt:1 4:1024:15:X-MOE:50B:ckpt:1    "
 # EP_BATCH_MAP["16:128:2"]="1:1024:15:X-MOE:190B:no-ckpt:0    1:1024:15:X-MOE:190B:ckpt:1 2:1024:15:X-MOE:190B:ckpt:1 4:1024:15:X-MOE:190B:ckpt:1   "
 
@@ -140,12 +163,23 @@ for node_key in "${!PP_STRATEGY_MAP[@]}"; do
         fi
 
         for batch_config in $batch_configs_string; do
-            IFS=':' read -r BS GBS TRAIN_ITERS MOE_TYPE MODEL_SIZE CHECKPOINT CHECKPOINT_NUM_LAYERS <<< "$batch_config"
+            IFS=':' read -r BS GBS TRAIN_ITERS MOE_TYPE MODEL_SIZE CHECKPOINT CHECKPOINT_NUM_LAYERS PP_PARTITION <<< "$batch_config"
 
             if [[ "$CHECKPOINT" == "ckpt" ]]; then
                 ACTIVATION_CHECKPOINT="true"
+                DYNAMIC_CHECKPOINT="False" # Capital F
+            elif [[ "$CHECKPOINT" == "dynamic-ckpt" ]]; then
+                ACTIVATION_CHECKPOINT="true"
+                DYNAMIC_CHECKPOINT="True" # Capital T
             else
                 ACTIVATION_CHECKPOINT="false"
+                DYNAMIC_CHECKPOINT="False" # Capital F
+            fi
+
+            if [[ "$PP_PARTITION" == "uneven" ]]; then
+                UNEVEN_PP_PARTITION="True" # Capital T
+            else
+                UNEVEN_PP_PARTITION="False" # Capital F
             fi
 
             RUN_TYPE="pp"
@@ -168,6 +202,8 @@ for node_key in "${!PP_STRATEGY_MAP[@]}"; do
                 -e "s/{{MODEL_SIZE}}/${MODEL_SIZE}/g" \
                 -e "s/{{ACTIVATION_CHECKPOINT}}/${ACTIVATION_CHECKPOINT}/g" \
                 -e "s/{{CHECKPOINT_NUM_LAYERS}}/${CHECKPOINT_NUM_LAYERS}/g" \
+                -e "s/{{DYNAMIC_CHECKPOINT}}/${DYNAMIC_CHECKPOINT}/g" \
+                -e "s/{{UNEVEN_PP_PARTITION}}/${UNEVEN_PP_PARTITION}/g" \
                 ${TEMPLATE_FILE} > ${TEMP_SLURM_SCRIPT}
 
             sbatch ${TEMP_SLURM_SCRIPT}
